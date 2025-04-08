0aafcb69c34602d179f15ff9126e3dcd3db24b3a
atthulurichetan <atthulurichetan@gmail.com>
2025-04-08 20:14:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <div className='App'>
+    <h1 className='text-red-400'>  Hello Chetan</h1>
+    </div>
+    
+  )
+}
+
+export default App
